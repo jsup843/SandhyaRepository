89faee6a32534e9db4a9bb10d208b2bbf51bc86c
SHREE <SHREE@SHREE-PC>
2020-08-24 08:52:33 -0700

    jo

--- EmployeeDetail/src/com/san/example/Employee.java
+++ EmployeeDetail/src/com/san/example/Employee.java
@@ -3,9 +3,7 @@ package com.san.example;
 public class Employee {
 
 	public static void main(String[] args) {
-		System.out.println("Hi, i am sandhya "); 
-		System.out.println("Hi, I am Supriya");
-		System.out.println("Hi, I am Shailaja");
+		System.out.println("Hi, i am sandhya ");  
 		
 
 	}
